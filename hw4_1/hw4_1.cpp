924bcb2d9c76ad7f9303530fa23c2168f11142c0
eunjun <eunjun@DESKTOP-5B870T5>
2023-10-03 03:15:27 +0900

    초기 커밋.

---
+++ hw4_1/hw4_1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+void main() {
+	int sum[15], N;
+	float root[15];
+	for (int i = 0; i < 15; i++) {
+		N = i + 1;
+		sum[i] = N * (N + 1) / 2;
+		root[i] = sqrt((float)N);
+
+	}
+	sum[10] = 2;
+	root[7] = 0.1;
+	for (int i = 0; i < 15; i++) {
+
+		cout << sum[i] << " " << root[i] << endl;
+	}
+}
